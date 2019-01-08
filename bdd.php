221333d5c2b20a26dbcc64af638f17d92dd7e77b
Beaurval <beaury.valentin@gmail.com>
2019-01-08 14:14:38 +0100

    first commit

---
+++ bdd.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Valentin Beaury
+ * Date: 08/01/2019
+ * Time: 14:03
+ */
+try {
+    $bdd = 'mysql:host=localhost;dbname=bdd';
+    $bdd_pdo = new PDO($bdd, 'root', 'root');
+}
+catch(Exeption $e){
+    die('Erreur :' . $e->getMessage());
+}
+$rep = $bdd_pdo->query('Select * from t_personnes');
+while ($data = $rep->fetch()){
+    echo $data['id']." ".$data["pernom"]." ".$data["perprenom"]." ".$data["perage"]."\n";
+}
