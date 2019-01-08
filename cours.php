ef161e9c775224fb75dfca4ab2d24b6b7533bdb6
Beaurval <beaury.valentin@gmail.com>
2019-01-08 09:00:53 +0100

    affichage des personnes

--- cours.php
+++ cours.php
@@ -7,20 +7,26 @@
  */
 $personne =
     array(
-        1=>array("nom" => "toto","prenom" => "tata","age"=>15),
-        2=>array("nom" => "tuche","prenom" => "katy","age"=>15),
-        3=>array("nom" => "bolor","prenom" => "arthur","age"=>15),
-        4=>array("nom" => "babar","prenom" => "alex","age"=>15),
-        5=>array("nom" => "smith","prenom" => "jojo","age"=>15),
-        6=>array("nom" => "bond","prenom" => "kante","age"=>15),
-        7=>array("nom" => "elves","prenom" => "benjamin","age"=>15),
-        8=>array("nom" => "matar","prenom" => "clement","age"=>15),
-        9=>array("nom" => "elis","prenom" => "mamadou","age"=>15),
-        10=>array("nom" => "elguapo","prenom" => "jean","age"=>15),
-        11=>array("nom" => "toredo","prenom" => "gérard","age"=>15),
-        12=>array("nom" => "lolo","prenom" => "titouan","age"=>15),
-        13=>array("nom" => "catapo","prenom" => "dan","age"=>15),
-        14=>array("nom" => "nino","prenom" => "pauline","age"=>15),
-        15=>array("nom" => "berdo","prenom" => "julie","age"=>15),
+        0=>array("nom" => "berdo","prenom" => "julie","age"=>15),
+        1=>array("nom" => "toto","prenom" => "tata","age"=>10),
+        2=>array("nom" => "tuche","prenom" => "katy","age"=>12),
+        3=>array("nom" => "bolor","prenom" => "arthur","age"=>28),
+        4=>array("nom" => "babar","prenom" => "alex","age"=>64),
+        5=>array("nom" => "smith","prenom" => "jojo","age"=>52),
+        6=>array("nom" => "bond","prenom" => "kante","age"=>37),
+        7=>array("nom" => "elves","prenom" => "benjamin","age"=>10),
+        8=>array("nom" => "matar","prenom" => "clement","age"=>5),
+        9=>array("nom" => "elis","prenom" => "mamadou","age"=>69),
+        10=>array("nom" => "elguapo","prenom" => "jean","age"=>52),
+        11=>array("nom" => "toredo","prenom" => "gérard","age"=>36),
+        12=>array("nom" => "lolo","prenom" => "titouan","age"=>20),
+        13=>array("nom" => "catapo","prenom" => "dan","age"=>18),
+        14=>array("nom" => "nino","prenom" => "pauline","age"=>3)
 );
+
+foreach ($personne as $pers){
+    foreach ($pers as $key=>$value){
+            echo $key." ".$value."\n";
+    }
+}
 
