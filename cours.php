45375a3759ce002d66cd56d8380770efa326a7a8
Beaurval <beaury.valentin@gmail.com>
2019-01-08 08:51:46 +0100

    création des personnes

--- cours.php
+++ cours.php
@@ -5,5 +5,22 @@
  * Date: 07/01/2019
  * Time: 14:34
  */
-$personnes = array("nom" => "toto","prenom" => "tata","age"=>15);
+$personne =
+    array(
+        1=>array("nom" => "toto","prenom" => "tata","age"=>15),
+        2=>array("nom" => "tuche","prenom" => "katy","age"=>15),
+        3=>array("nom" => "bolor","prenom" => "arthur","age"=>15),
+        4=>array("nom" => "babar","prenom" => "alex","age"=>15),
+        5=>array("nom" => "smith","prenom" => "jojo","age"=>15),
+        6=>array("nom" => "bond","prenom" => "kante","age"=>15),
+        7=>array("nom" => "elves","prenom" => "benjamin","age"=>15),
+        8=>array("nom" => "matar","prenom" => "clement","age"=>15),
+        9=>array("nom" => "elis","prenom" => "mamadou","age"=>15),
+        10=>array("nom" => "elguapo","prenom" => "jean","age"=>15),
+        11=>array("nom" => "toredo","prenom" => "gérard","age"=>15),
+        12=>array("nom" => "lolo","prenom" => "titouan","age"=>15),
+        13=>array("nom" => "catapo","prenom" => "dan","age"=>15),
+        14=>array("nom" => "nino","prenom" => "pauline","age"=>15),
+        15=>array("nom" => "berdo","prenom" => "julie","age"=>15),
+);
 
