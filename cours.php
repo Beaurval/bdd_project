6fa5b48b83512e37e81a18313dd32c4666582eff
Beaurval <beaury.valentin@gmail.com>
2019-01-08 09:23:13 +0100

    gestion des entrées utilisateur

--- cours.php
+++ cours.php
@@ -7,24 +7,39 @@
  */
 
 //création des personnes
-$personne =
-    array(
-        0=>array("nom" => "berdo","prenom" => "julie","age"=>15),
-        1=>array("nom" => "toto","prenom" => "tata","age"=>10),
-        2=>array("nom" => "tuche","prenom" => "katy","age"=>12),
-        3=>array("nom" => "bolor","prenom" => "arthur","age"=>28),
-        4=>array("nom" => "babar","prenom" => "alex","age"=>64),
-        5=>array("nom" => "smith","prenom" => "jojo","age"=>52),
-        6=>array("nom" => "bond","prenom" => "kante","age"=>37),
-        7=>array("nom" => "elves","prenom" => "benjamin","age"=>10),
-        8=>array("nom" => "matar","prenom" => "clement","age"=>5),
-        9=>array("nom" => "elis","prenom" => "mamadou","age"=>69),
-        10=>array("nom" => "elguapo","prenom" => "jean","age"=>52),
-        11=>array("nom" => "toredo","prenom" => "gérard","age"=>36),
-        12=>array("nom" => "lolo","prenom" => "titouan","age"=>20),
-        13=>array("nom" => "catapo","prenom" => "dan","age"=>18),
-        14=>array("nom" => "nino","prenom" => "pauline","age"=>3)
-);
+$mode = readline("Voulez vous spécifier le nom des personnes vous-même ? O/N");
+if ($mode == 'N' || $mode == 'n'){
+    $personne =
+        array(
+            0=>array("nom" => "berdo","prenom" => "julie","age"=>15),
+            1=>array("nom" => "toto","prenom" => "tata","age"=>10),
+            2=>array("nom" => "tuche","prenom" => "katy","age"=>12),
+            3=>array("nom" => "bolor","prenom" => "arthur","age"=>28),
+            4=>array("nom" => "babar","prenom" => "alex","age"=>64),
+            5=>array("nom" => "smith","prenom" => "jojo","age"=>52),
+            6=>array("nom" => "bond","prenom" => "kante","age"=>37),
+            7=>array("nom" => "elves","prenom" => "benjamin","age"=>10),
+            8=>array("nom" => "matar","prenom" => "clement","age"=>5),
+            9=>array("nom" => "elis","prenom" => "mamadou","age"=>69),
+            10=>array("nom" => "elguapo","prenom" => "jean","age"=>52),
+            11=>array("nom" => "toredo","prenom" => "gérard","age"=>36),
+            12=>array("nom" => "lolo","prenom" => "titouan","age"=>20),
+            13=>array("nom" => "catapo","prenom" => "dan","age"=>18),
+            14=>array("nom" => "nino","prenom" => "pauline","age"=>3)
+        );
+}
+else{
+    $nbPers = readline("Combien de personne voulez-vous renseigner ? (entrez un nombre) ");
+    for ($i = 0; $i<$nbPers; $i++){
+        $personne[$i]["nom"] = readline("prenom personne ".($i+1)." : ");
+        $personne[$i]["prenom"] = readline("nom personne ".($i+1)." : ");
+        $personne[$i]["age"] = readline("age personne ".($i+1)." : ");
+        echo "\n";
+    }
+}
+echo "\n";
+echo "Affichage des personnes de + de 18 ans";
+echo "\n";
 
 foreach ($personne as $pers){
     $flag = 0;
@@ -37,4 +52,3 @@ foreach ($personne as $pers){
     if ($flag == 0)
         echo "\n";
 }
-
